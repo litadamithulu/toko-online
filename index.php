f2e739b56894a1b5975fc565e868076d4e378341
unknown <maruf.plh@gmail.com>
2021-11-29 13:34:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,188 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Toko Online | Aksesoris Hp </title>
+    <style>
+      btn {
+        .border-radius : 0 !important;
+      }
+      .from-control {
+        border-radius: 0 !important;
+      }
+      .card img {
+            width:200px;
+            height:200px;
+      }
+      </style>
+  </head>
+  <body>
+      <nav class="navbar  fixed-top navbar-expand-lg navbar-light bg-light bg-success">
+  <div class="container">
+    <a class="navbar-brand" href="#">toko |you-want </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+         </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-danger" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class ="bg-secondary bg-opacity-10 pb-5 mt-5">
+    <div class="container pt-3">
+  <h4 class="mb-4">barang terbaru</h4>
+  <div class="d-flex justify-content-start">
+  <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+<img src="gambar/tripod.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Tripod hp dan CamDic</h5>
+      <p class="card-teks">Rp.150.ooo</p>
+<a href="#" class="btn btn-primary">beli</a>
+      </div>
+      </div>
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/tripod1.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Tripod Pendek</h5>
+      <p class="card-teks">Rp100.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>  
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/headset.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Headset Besar</h5>
+      <p class="card-teks">Rp250.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/headset2.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Headset Sony</h5>
+      <p class="card-teks">Rp150.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/headset2.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Headset Sony</h5>
+      <p class="card-teks">Rp150.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      <! buat div barang terlaris...>
+      <div>
+        <! buat div yang kelasnya container >
+        <div class="container pt-3">
+          <h4 class="mb-4">barang terlaris saat ini</h4>
+          <!masukkan card di dalam container >
+          <div class="d-flex justify-content-start">
+          <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/tripod1.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Tripod Pendek</h5>
+      <p class="card-teks">Rp100.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>  
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+<img src="gambar/tripod.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Tripod hp dan CamDic</h5>
+      <p class="card-teks">Rp.150.ooo</p>
+<a href="#" class="btn btn-primary">beli</a>
+      </div>
+      </div> 
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/headset.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Headset Besar</h5>
+      <p class="card-teks">Rp250.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>
+      <div class="card me-1"
+    style="width: 18rem;">
+<div class="d-flex justify-content-center">
+  <img src="gambar/headset2.jpg" alt="Tripod">
+      </div>
+  <div class="card-body">
+      <h5 class="card-title">Headset Sony</h5>
+      <p class="card-teks">Rp150.000</p>
+<a href="#" class="btn btn-primary">Beli</a>
+      </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      <!  testimony  >
+      <div class="bg-secondary bg-opacity-10 pb-5 mt-5">
+        <h4 class="mb-4">testimoni pelanggan </h4>
+        <p>saya sangat puas belanja di toko ini</p>
+        <p>saya sangat puas belanja di toko ini</p>
+        <p>saya sangat puas belanja di toko ini</p>
+        <p>saya sangat puas belanja di toko ini</p>
+      </div>
+      </div>
+
+     
+      
+     
+
+   
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
